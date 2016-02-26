c464b676cad6272414ab4617b3e94496b0022cb7
Benjamin Downs <benjamin.downs@gmail.com>
2016-02-26 15:58:03 +0100

    add directory structure; working on homepage

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express')
 var app = express()
 var timeService = require('./timeService.js')
+var path = require('path')
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/:time',timeService);
+
+app.get('*', function(req, res) {
+    res.render('index.html')
+})
 
 app.listen(3000)
--- timeService.js
+++ timeService.js
@@ -1,43 +1,45 @@
 
 
 module.exports = function (req, res) {
+
     var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    var year, monthIndex, dayIndex, finalDate, unixTime, dateTime
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var year, monthIndex, dayIndex, finalDate, unixTime, dateTime;
 
     var time = req.params.time
 
     if (!time) {
-        res.send('try again')
+        
+        res.send('no time designated')
 
     } else {
         try {
             if (/^\d*$/.test(time)) {
                 dateTime = new Date(time * 1000)
-                unixTime = time
+                unixTime = time;
             } else {
-                dateTime = new Date(time)
+                dateTime = new Date(time);
                 unixTime = dateTime.getTime() / 1000
             }
 
-            year = dateTime.getFullYear()
-            monthIndex = dateTime.getMonth()
-            dayNum = dateTime.getDate()
-            dayIndex = dateTime.getDay()
+            year = dateTime.getFullYear();
+            monthIndex = dateTime.getMonth();
+            dayNum = dateTime.getDate();
+            dayIndex = dateTime.getDay();
 
             finalDate = {
                 unix: unixTime,
                 readable: days[dayIndex] + ", " + months[monthIndex] + " " + dayNum + ", " + year
-            }
+            };
 
             if (!finalDate.unix || !finalDate.readable) {
-                throw err
+                throw err;
             }
 
-            res.send(finalDate)
+            res.send(finalDate);
 
         } catch (err) {
-            res.send("It looks like your date was not properly formatted. Try MM-DD-YYYY or 'January 15 2014' for best results.")
+            res.send("It looks like your date was not properly formatted. Try MM-DD-YYYY or 'January 15 2014' for best results.");
         }
     }
-}
+};
